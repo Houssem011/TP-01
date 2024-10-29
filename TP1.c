05f4d6a0b1824e1843b6dee381cb4cbc43d787a7
Houssem011 <houssamarg3@gmail.com>
2024-10-29 10:26:46 +0100

    Update TP1.c

--- TP1.c
+++ TP1.c
@@ -56,3 +56,56 @@ int main() {
 
     return 0;
 }
+
+// Exercice02
+
+
+#include <stdio.h>
+
+// Fonction pour échanger les triangles supérieur et inférieur
+void echanger_triangles(int matrice[4][4], int taille) {
+    int i, j;
+    for (i = 0; i < taille; i++) {
+        for (j = i + 1; j < taille; j++) {
+            // Échanger matrice[i][j] avec matrice[j][i]
+            int temp = matrice[i][j];
+            matrice[i][j] = matrice[j][i];
+            matrice[j][i] = temp;
+        }
+    }
+}
+
+int main() {
+    // Initialiser la matrice
+    int matrice[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    int i, j;
+
+    // Afficher la matrice avant l'échange
+    printf("Matrice avant l'échange des triangles :\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", matrice[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Effectuer l'échange
+    echanger_triangles(matrice, 4);
+
+    // Afficher la matrice après l'échange
+    printf("\nMatrice après l'échange des triangles :\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", matrice[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
